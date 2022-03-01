dfc23240f8eef41aba2340d7a72a565323f66675
Garn123 <garnyg@hotmail.com>
2022-03-01 14:52:45 +0000

    Iteration#2

---
+++ Iteration-one.js
@@ -0,0 +1,6 @@
+function sum(numberOne , numberTwo) {
+    ( numberOne > numberTwo) ? console.log(numberOne + " es mayor que " + numberTwo) : console.log(numberTwo + " es mayor que " + numberOne);
+}
+
+sum(8, 5);
+
